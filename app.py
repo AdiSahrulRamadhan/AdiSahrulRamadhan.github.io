b4f424d4cc9f47a05f310f084e7b6af61391f850
AdiSahrulRamadhan <160815677+AdiSahrulRamadhan@users.noreply.github.com>
2024-06-12 11:41:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify, session
 from werkzeug.utils import secure_filename
 from extract_features import extract_features
 import os
@@ -14,7 +14,7 @@ import logging
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'C:/Users/Adi Sahrul R/OneDrive/Documents/Penambangan Data/uploads'
+UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 logging.basicConfig(level=logging.DEBUG)
@@ -125,19 +125,31 @@ def train_meta_classifier():
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+has_extraction_result = False
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if 'image' not in request.files:
-        return redirect(request.url)
-    file = request.files['image']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        features = extract_features(file_path)
-        return render_template('index.html', features=features, image_name=filename)
+    global has_extraction_result
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return redirect(request.url)
+        file = request.files['image']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            features = extract_features(file_path)
+            has_extraction_result = True
+            return render_template('index.html', features=features, image_name=filename)
+    # Jika hasil ekstraksi sudah ada, tetap tampilkan halaman upload
+    if has_extraction_result:
+        has_extraction_result = False
+        return render_template('index.html')
+    # Jika tidak, arahkan pengguna kembali ke halaman utama
+    return redirect(url_for('index'))
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -188,4 +200,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
